dcfd6f68c4f065685bce5fc4c3cd77f6d8842bf2
gldeA <glde.abbott@gmail.com>
2026-02-06 17:19:31 -0600

    Unstaged changes during deploy at 20260206_171930

--- src/main/java/frc/robot/TunerConstants.java
+++ src/main/java/frc/robot/TunerConstants.java
@@ -30,11 +30,11 @@ public class TunerConstants {
   // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
   private static final Slot0Configs steerGains =
       new Slot0Configs()
-          .withKP(0.1)
+          .withKP(10)
           .withKI(0)
           .withKD(0)
           .withKS(0)
-          .withKV(.1)
+          .withKV(2.5)
           .withKA(0)
           .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
   // When using closed-loop control, the drive motor uses the control
