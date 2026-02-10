47ceda937c0510dc31488bf01cb7a67b01ab7abc
gldeA <glde.abbott@gmail.com>
2026-02-09 20:40:16 -0600

    It turns now, not done tuning yet

--- src/main/java/frc/robot/TunerConstants.java
+++ src/main/java/frc/robot/TunerConstants.java
@@ -28,13 +28,14 @@ public class TunerConstants {
   // The steer motor uses any SwerveModule.SteerRequestType control request with
   // the
   // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
+  // PID defined by mechanism, not rotor (0.1 = 10)
   private static final Slot0Configs steerGains =
       new Slot0Configs()
-          .withKP(0.1)
+          .withKP(25)
           .withKI(0)
           .withKD(0)
           .withKS(0)
-          .withKV(.1)
+          .withKV(0)
           .withKA(0)
           .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
   // When using closed-loop control, the drive motor uses the control
@@ -44,7 +45,8 @@ public class TunerConstants {
 
   // The closed-loop output type to use for the steer motors;
   // This affects the PID/FF gains for the steer motors
-  private static final ClosedLoopOutputType kSteerClosedLoopOutput = ClosedLoopOutputType.Voltage;
+  private static final ClosedLoopOutputType kSteerClosedLoopOutput =
+      ClosedLoopOutputType.TorqueCurrentFOC;
   // The closed-loop output type to use for the drive motors;
   // This affects the PID/FF gains for the drive motors
   private static final ClosedLoopOutputType kDriveClosedLoopOutput = ClosedLoopOutputType.Voltage;
@@ -100,7 +102,7 @@ public class TunerConstants {
   private static final double kSteerGearRatio = 18.75;
   private static final Distance kWheelRadius = Inches.of(2);
 
-  private static final boolean kInvertLeftSide = false;
+  private static final boolean kInvertLeftSide = true;
   private static final boolean kInvertRightSide = true;
 
   private static final int kPigeonId = 1;
