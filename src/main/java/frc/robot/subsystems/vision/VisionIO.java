139dc0347430d3113a9d4dead811a8e1893bd9b8
linnea <lkennerhed@gmail.com>
2026-02-13 18:48:07 -0600

    Convert camera stuff to AdvantageKit

--- src/main/java/frc/robot/subsystems/vision/VisionIO.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIO.java
@@ -1,36 +1,18 @@
 package frc.robot.subsystems.vision;
 
-import edu.wpi.first.math.geometry.Pose3d;
-import java.util.Optional;
-import org.littletonrobotics.junction.LogTable;
-import org.littletonrobotics.junction.inputs.LoggableInputs;
+import org.littletonrobotics.junction.AutoLog;
 
 /** The IO layer for one camera */
 public interface VisionIO {
   /** The inputs from a camera */
-  class VisionIOInputs implements LoggableInputs {
-    public Optional<Pose3d> pose = Optional.empty();
-    public Optional<Double> timestamp = Optional.empty();
+  @AutoLog
+  class VisionIOInputs {
 
-    @Override
-    public void toLog(LogTable table) {
-      table.put("hasPose", pose.isPresent());
-      table.put("hasTimestamp", timestamp.isPresent());
-      table.put("Pose", pose.orElse(new Pose3d()));
-      table.put("Timestamp", timestamp.orElse(0.0));
-    }
-
-    @Override
-    public void fromLog(LogTable table) {
-      pose =
-          table.get("hasPose", false)
-              ? Optional.of(table.get("Pose", new Pose3d()))
-              : Optional.empty();
-      timestamp =
-          table.get("hasTimestamp", false)
-              ? Optional.of(table.get("Timestamp", 0.0))
-              : Optional.empty();
-    }
+    public int[] idNumber = new int[] {};
+    public double[] yaw = new double[] {};
+    public double[] pitch = new double[] {};
+    public double[] roll = new double[] {};
+    public double[] area = new double[] {};
   }
 
   /**
