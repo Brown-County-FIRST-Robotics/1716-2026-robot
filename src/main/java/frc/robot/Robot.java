d061f9fe77905c05c99cd35e14158d090f3ff47e
cfinn <cfinn68916@proton.me>
2026-03-06 16:18:46 -0600

    Run the periodic runnables
    Signed-off-by: cfinn <cfinn68916@proton.me>

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import java.io.File;
+import frc.robot.utils.PeriodicRunnable;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import org.littletonrobotics.junction.LogFileUtil;
@@ -105,6 +106,7 @@ public class Robot extends LoggedRobot {
     // This must be called from the robot's periodic block in order for anything in
     // the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    PeriodicRunnable.runPeriodic();
 
     // Return to non-RT thread priority (do not modify the first argument)
     // Threads.setCurrentThreadPriority(false, 10);
