d08dcb77a288f6caf0bb6355649a85aead503ae3
Quinniboi10 <69563433+Quinniboi10@users.noreply.github.com>
2026-03-07 11:47:29 -0600

    Unstaged changes during deploy at 20260307_114728

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -81,8 +81,8 @@ public class RobotContainer {
                 new ModuleIOTalonFX(TunerConstants.BackLeft),
                 new ModuleIOTalonFX(TunerConstants.BackRight));
         shooter = new Shooter(new ShooterIOKrakens(62, 9));
-        rollers = new Rollers(new RollersIOKraken(new CANBus("1716_canivore"), 0, 35));
-        intake = new Intake(new IntakeIOKraken(new CANBus("1716_canivore"), -1, -1));
+        rollers = new Rollers(new RollersIOKraken(new CANBus("1716_canivore"), -1, 37));
+        intake = new Intake(new IntakeIOKraken(new CANBus("1716_canivore"), 40, -1));
 
         // The ModuleIOTalonFXS implementation provides an example implementation for
         // TalonFXS controller connected to a CANdi with a PWM encoder. The
@@ -225,8 +225,8 @@ public class RobotContainer {
     opcon.x().whileTrue(Commands.run(() -> shooter.quickServoCommand(2), shooter));
     opcon.y().whileTrue(Commands.run(() -> shooter.quickServoCommand(0), shooter));
 
-    opcon.leftBumper().whileTrue(Commands.run(() -> rollers.jset(4), rollers));
-    opcon.rightBumper().whileTrue(Commands.run(() -> rollers.jset(-4), rollers));
+    // opcon.leftBumper().whileTrue(Commands.run(() -> rollers.jset(4), rollers));
+    // opcon.rightBumper().whileTrue(Commands.run(() -> rollers.jset(-4), rollers));
     opcon
         .leftStick()
         .whileTrue(
@@ -257,10 +257,11 @@ public class RobotContainer {
                 shooter));
 
     // Intake/hopper control
-    controller.rightBumper().whileTrue(intake.extendHopper());
-    controller.leftBumper().whileTrue(intake.retractHopper());
-    controller.rightBumper().whileTrue(intake.intake());
-    controller.leftBumper().whileTrue(intake.intakeReverse());
+    // controller.rightBumper().whileTrue(intake.extendHopper());
+    // controller.leftBumper().whileTrue(intake.retractHopper());
+    // controller.rightBumper().whileTrue(intake.intake());
+    // controller.leftBumper().whileTrue(intake.intakeReverse());
+    controller.leftBumper().whileTrue(Commands.print("Left bumper"));
   }
 
   /**
