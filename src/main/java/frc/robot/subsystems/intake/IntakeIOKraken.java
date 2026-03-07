c9ccc56462db9645a6079b50ebb36ac17417bd2d
Quinniboi10 <69563433+Quinniboi10@users.noreply.github.com>
2026-03-07 15:12:06 -0600

    Unstaged changes during deploy at 20260307_151205

--- src/main/java/frc/robot/subsystems/intake/IntakeIOKraken.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOKraken.java
@@ -4,9 +4,11 @@ import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.ClosedLoopRampsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
@@ -24,22 +26,22 @@ public class IntakeIOKraken implements IntakeIO {
 
   public IntakeIOKraken(CANBus canbus, int intakeID, int extendID) {
     intakeMotor = new TalonFX(intakeID, canbus);
-    extendMotor = new TalonFX(extendID, canbus);
+    // extendMotor = new TalonFX(extendID, canbus);
     intakeVelocity = intakeMotor.getVelocity();
     intakeCurrent = intakeMotor.getStatorCurrent();
     intakeCurrent.setUpdateFrequency(50);
     intakeAppliedVolts = intakeMotor.getMotorVoltage();
-    extendVelocity = extendMotor.getVelocity();
-    extendCurrent = extendMotor.getStatorCurrent();
-    extendCurrent.setUpdateFrequency(50);
-    extendAppliedVolts = extendMotor.getMotorVoltage();
-    var extend_cfgr = extendMotor.getConfigurator();
-    extend_cfgr.apply(
-        new ClosedLoopRampsConfigs()
-            .withDutyCycleClosedLoopRampPeriod(0.5)
-            .withTorqueClosedLoopRampPeriod(0.5));
-    extend_cfgr.apply(
-        new Slot0Configs().withKV(12.0 / (7758.0 / 60.0)).withKP(1.2 * 12.0 / (7758.0 / 60.0)));
+    // extendVelocity = extendMotor.getVelocity();
+    // extendCurrent = extendMotor.getStatorCurrent();
+    // extendCurrent.setUpdateFrequency(50);
+    // extendAppliedVolts = extendMotor.getMotorVoltage();
+    // var extend_cfgr = extendMotor.getConfigurator();
+    // extend_cfgr.apply(
+    //     new ClosedLoopRampsConfigs()
+    //         .withDutyCycleClosedLoopRampPeriod(0.5)
+    //         .withTorqueClosedLoopRampPeriod(0.5));
+    // extend_cfgr.apply(
+    //     new Slot0Configs().withKV(12.0 / (7758.0 / 60.0)).withKP(1.2 * 12.0 / (7758.0 / 60.0)));
     var intake_cfgr = intakeMotor.getConfigurator();
     intake_cfgr.apply(
         new ClosedLoopRampsConfigs()
@@ -47,19 +49,20 @@ public class IntakeIOKraken implements IntakeIO {
             .withTorqueClosedLoopRampPeriod(0.5));
     intake_cfgr.apply(
         new Slot0Configs().withKV(12.0 / (7758.0 / 60.0)).withKP(0.8 * 12.0 / (7758.0 / 60.0)));
+    intake_cfgr.apply(new MotorOutputConfigs().withInverted(InvertedValue.Clockwise_Positive));
   }
 
   @Override
   public void updateInputs(IntakeIOInputs inputs) {
     var intakeStatus =
         BaseStatusSignal.refreshAll(intakeCurrent, intakeAppliedVolts, intakeVelocity);
-    var extendStatus =
-        BaseStatusSignal.refreshAll(extendCurrent, extendAppliedVolts, extendVelocity);
-    inputs.extendConnected = extendStatus.isOK();
-    inputs.extendConnected = intakeStatus.isOK();
-    inputs.extendAppliedVolts = extendAppliedVolts.getValueAsDouble();
-    inputs.extendVelocity = extendVelocity.getValue().in(Units.RotationsPerSecond);
-    inputs.extendAppliedCurrent = extendCurrent.getValueAsDouble();
+    // var extendStatus =
+    //     BaseStatusSignal.refreshAll(extendCurrent, extendAppliedVolts, extendVelocity);
+    // inputs.extendConnected = extendStatus.isOK();
+    // inputs.extendConnected = intakeStatus.isOK();
+    // inputs.extendAppliedVolts = extendAppliedVolts.getValueAsDouble();
+    // inputs.extendVelocity = extendVelocity.getValue().in(Units.RotationsPerSecond);
+    // inputs.extendAppliedCurrent = extendCurrent.getValueAsDouble();
     inputs.intakeAppliedVolts = intakeAppliedVolts.getValueAsDouble();
     inputs.intakeVelocity = intakeVelocity.getValue().in(Units.RotationsPerSecond);
     inputs.intakeAppliedCurrent = intakeCurrent.getValueAsDouble();
@@ -72,6 +75,6 @@ public class IntakeIOKraken implements IntakeIO {
 
   @Override
   public void extendSpeed(double extend_vel) {
-    extendMotor.setControl(new VelocityVoltage(extend_vel));
+    // extendMotor.setControl(new VelocityVoltage(extend_vel));
   }
 }
--- src/main/java/frc/robot/subsystems/vision/Quest.java
+++ src/main/java/frc/robot/subsystems/vision/Quest.java
@@ -1,17 +1,20 @@
 package frc.robot.subsystems.vision;
 
 import edu.wpi.first.math.geometry.*;
-import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.utils.PeriodicRunnable;
 import org.littletonrobotics.junction.Logger;
 
-public class Quest implements Subsystem {
+public class Quest extends PeriodicRunnable {
   QuestIO io;
   QuestIOInputsAutoLogged inputs = new QuestIOInputsAutoLogged();
   private static final Transform3d posOnRobot =
-      new Transform3d(new Translation3d(-0.2, -0.25, 0.5), new Rotation3d(0, 0, Math.PI * 1.1));
+      new Transform3d(
+          new Translation3d(-10.59 * 0.0254, -12.37 * 0.0254, 0.5),
+          new Rotation3d(0, 0, 200.0 * Math.PI / 180.0));
   private Pose3d realPose = new Pose3d();
 
   public Quest(QuestIO io) {
+    super();
     this.io = io;
   }
 
