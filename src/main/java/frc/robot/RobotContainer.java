9a18b839de5dadc555df972d0e50d503664675d5
Quinniboi10 <69563433+Quinniboi10@users.noreply.github.com>
2026-03-13 20:28:13 -0500

    Unstaged changes during deploy at 20260313_202813

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,7 +15,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -225,28 +224,17 @@ public class RobotContainer {
                 .ignoringDisable(true));
 
     // Hood positions
-    controller
-        .a()
-        .whileTrue(
-            Commands.run(
-                () -> shooter.quickServoCommand(1),
-                shooter));
+    controller.a().whileTrue(Commands.run(() -> shooter.quickServoCommand(2), shooter));
     controller.y().whileTrue(Commands.run(() -> shooter.quickServoCommand(0), shooter));
 
     // Press right trigger to run shooter startup
     controller
         .rightTrigger(0.7)
         .whileTrue(
-            Commands.run(
-                    () -> shooter.setShooterSpeed(80),
-                    shooter)
+            Commands.run(() -> shooter.setShooterSpeed(80), shooter)
                 .alongWith(
                     Commands.waitSeconds(1.0)
-                        .andThen(
-                            Commands.run(
-                                () ->
-                                    rollers.setSpeeds(0, 20),
-                                rollers)))
+                        .andThen(Commands.run(() -> rollers.setSpeeds(0, 20), rollers)))
                 .alongWith(
                     Commands.race(
                             Commands.run(() -> controller.setRumble(RumbleType.kBothRumble, 0.5)),
