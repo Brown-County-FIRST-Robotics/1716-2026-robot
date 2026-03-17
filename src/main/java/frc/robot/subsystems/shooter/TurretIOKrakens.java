565f6c6537ebc9266b7dfea304ffea2a05601a05
cfinn <cfinn68916@proton.me>
2026-03-17 16:18:31 -0500

    Unstaged changes during deploy at 20260317_161831

--- src/main/java/frc/robot/subsystems/shooter/TurretIOKrakens.java
+++ src/main/java/frc/robot/subsystems/shooter/TurretIOKrakens.java
@@ -6,6 +6,8 @@ import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
 import frc.robot.OurConstants;
 
 public class TurretIOKrakens implements TurretIO {
@@ -14,6 +16,8 @@ public class TurretIOKrakens implements TurretIO {
   CANcoder encoderB;
   StatusSignal<Angle> a_angle;
   StatusSignal<Angle> b_angle;
+  StatusSignal<Current> turretCurrent;
+  StatusSignal<Voltage> turretAppliedOutput;
 
   public TurretIOKrakens(int motorID, int encoder_a_id, int encoder_b_id) {
     motor = new TalonFX(motorID, OurConstants.CAN_BUS);
@@ -21,6 +25,7 @@ public class TurretIOKrakens implements TurretIO {
     encoderB = new CANcoder(encoder_b_id, OurConstants.CAN_BUS);
     a_angle = encoderA.getAbsolutePosition();
     b_angle = encoderB.getAbsolutePosition();
+    motor.getPosition();
   }
 
   @Override
