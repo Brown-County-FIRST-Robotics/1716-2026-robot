15314713abd6cbefbe09ca7d92b78832325084d8
Quinniboi10 <69563433+Quinniboi10@users.noreply.github.com>
2026-03-20 19:53:09 -0500

    Unstaged changes during deploy at 20260320_195308

--- src/main/java/frc/robot/subsystems/intake/IntakeIOKraken.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOKraken.java
@@ -39,7 +39,7 @@ public class IntakeIOKraken implements IntakeIO {
     extendAppliedVolts = extendMotor.getMotorVoltage();
     var extend_cfgr = extendMotor.getConfigurator();
 
-    extend_cfgr.apply(new Slot0Configs().withKV(0.12).withKP(3).withKS(1));
+    extend_cfgr.apply(new Slot0Configs().withKP(0.12).withKV(0.12 * 0.5).withKS(1));
     extend_cfgr.apply(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake));
     var intake_cfgr = intakeMotor.getConfigurator();
     intake_cfgr.apply(
