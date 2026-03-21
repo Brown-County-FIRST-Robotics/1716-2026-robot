0b1e42f6c4f605bce1d8329894d16fe0471ba214
Quinniboi10 <69563433+Quinniboi10@users.noreply.github.com>
2026-03-21 14:57:00 -0500

    Unstaged changes during deploy at 20260321_145659

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -219,6 +219,14 @@ public class RobotContainer {
             () -> -controller.getLeftY() / (controller.leftTrigger().getAsBoolean() ? 2 : 1),
             () -> -controller.getLeftX() / (controller.leftTrigger().getAsBoolean() ? 2 : 1),
             () -> -controller.getRightX() / (controller.leftTrigger().getAsBoolean() ? 2 : 1)));
+    opcon
+        .a()
+        .onTrue(
+            DriveCommands.joystickDriveRobotRelative(
+                drive,
+                () -> -controller.getLeftY() / (controller.leftTrigger().getAsBoolean() ? 2 : 1),
+                () -> -controller.getLeftX() / (controller.leftTrigger().getAsBoolean() ? 2 : 1),
+                () -> -controller.getRightX() / (controller.leftTrigger().getAsBoolean() ? 2 : 1)));
 
     // Lock to 0° when A button is held
     //    controller
@@ -247,7 +255,6 @@ public class RobotContainer {
     // Hood positions
     controller.y().whileTrue(Commands.run(() -> shooter.quickServoCommand(0.2), shooter));
     controller.b().whileTrue(Commands.run(() -> shooter.quickServoCommand(1), shooter));
-    controller.a().whileTrue(Commands.run(() -> shooter.quickServoCommand(1.75), shooter));
 
     // Reset initial pos on auto init
     RobotModeTriggers.autonomous()
@@ -289,7 +296,15 @@ public class RobotContainer {
                 () ->
                     shooter.commandTurret(
                         shooter.getTurretRotation().plus(Rotation2d.fromRotations(-0.1)))));
-    opcon.x().whileTrue(Commands.run(() -> shooter.commandTurretToTrack(drive.getPose()), shooter));
+    opcon
+        .x()
+        .whileTrue(
+            Commands.runEnd(
+                () -> shooter.commandTurretToTrack(drive.getPose()),
+                () -> shooter.commandTurret(Rotation2d.k180deg)));
+    opcon.y().whileTrue(Commands.run(() -> shooter.quickServoCommand(0.2), shooter));
+    opcon.b().whileTrue(Commands.run(() -> shooter.quickServoCommand(1), shooter));
+    opcon.a().whileTrue(Commands.run(() -> shooter.quickServoCommand(1.75), shooter));
 
     // Intake/hopper control
     opcon.rightTrigger().whileTrue(intake.extendHopperVelocity());
