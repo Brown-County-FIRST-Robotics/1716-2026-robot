cc948680da8fbadd13470ba1a35b67ade96cd9d3
Quinniboi10 <69563433+Quinniboi10@users.noreply.github.com>
2026-03-21 12:38:16 -0500

    Unstaged changes during deploy at 20260321_123815

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -53,19 +53,35 @@ public class Intake extends SubsystemBase {
   }
 
   public Command intake() {
-    return Commands.run(() -> io.intakeSpeed(150), this);
+    return Commands.runEnd(() -> io.intakeSpeed(60), () -> io.intakeSpeed(0), this);
+  }
+
+  public Command intakeReverse() {
+    return Commands.runEnd(() -> io.intakeSpeed(-25), () -> io.intakeSpeed(0), this);
   }
 
   public Command intakeStop() {
     return Commands.run(() -> io.intakeSpeed(0), this);
   }
 
-  public Command intakeReverse() {
-    return Commands.run(() -> io.intakeSpeed(-100), this);
+  public Command extendHopper() {
+    return Commands.run(() -> io.extenderPosition(extendedZeroPosition + 14.1), this);
   }
 
   public Command retractHopper() {
     return Commands.run(() -> io.extenderPosition(extendedZeroPosition), this);
+  }
+
+  public Command extendHopperVelocity() {
+    return Commands.runEnd(() -> io.extenderVelocity(4), () -> io.extenderVelocity(0), this);
+  }
+
+  public Command retractHopperVelocity() {
+    return Commands.runEnd(() -> io.extenderVelocity(-5), () -> io.extenderVelocity(0), this);
+  }
+
+  public Command retractStop() {
+    return Commands.run(() -> io.extenderVelocity(0), this);
   }
 
   public boolean isExtenderConnected() {
