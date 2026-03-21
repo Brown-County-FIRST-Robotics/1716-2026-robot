37e8f6e3d979a435ef158d6d80e93ebe3770c008
Quinniboi10 <69563433+Quinniboi10@users.noreply.github.com>
2026-03-21 08:18:27 -0500

    Unstaged changes during deploy at 20260321_081827

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -61,11 +61,27 @@ public class Intake extends SubsystemBase {
   }
 
   public Command intakeReverse() {
-    return Commands.run(() -> io.intakeSpeed(-100), this);
+    return Commands.run(() -> io.intakeSpeed(-25), this);
+  }
+
+  public Command extendHopper() {
+    return Commands.run(() -> io.extenderPosition(extendedZeroPosition + 14.1), this);
   }
 
   public Command retractHopper() {
     return Commands.run(() -> io.extenderPosition(extendedZeroPosition), this);
+  }
+
+  public Command extendHopperVelocity() {
+    return Commands.run(() -> io.extenderVelocity(0.1), this);
+  }
+
+  public Command retractHopperVelocity() {
+    return Commands.run(() -> io.extenderVelocity(-0.4), this);
+  }
+
+  public Command retractStop() {
+    return Commands.run(() -> io.extenderVelocity(0), this);
   }
 
   public boolean isExtenderConnected() {
