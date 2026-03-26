a62f95f86dd15ad23cfeebad8c8137d564ac603f
Quinniboi10 <69563433+Quinniboi10@users.noreply.github.com>
2026-03-25 20:02:45 -0500

    Unstaged changes during deploy at 20260325_200245

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -271,12 +271,12 @@ public class RobotContainer extends PeriodicRunnable {
                     },
                     shooter)
                 .alongWith(
-                    Commands.waitSeconds(0.3)
+                    Commands.waitSeconds(0.4)
                         .andThen(Commands.run(() -> rollers.setSpeeds(20, 20), rollers)))
                 .alongWith(
                     Commands.race(
                             Commands.run(() -> controller.setRumble(RumbleType.kRightRumble, 0.5)),
-                            Commands.waitSeconds(0.275))
+                            Commands.waitSeconds(0.375))
                         .andThen(
                             Commands.run(() -> controller.setRumble(RumbleType.kBothRumble, 1.0))))
                 .finallyDo(
@@ -309,10 +309,10 @@ public class RobotContainer extends PeriodicRunnable {
     opcon.b().whileTrue(Commands.run(() -> shooter.quickServoCommand(1), shooter));
 
     // Intake/hopper control
-    opcon.rightTrigger().whileTrue(intake.extendHopperVelocity());
-    opcon.leftTrigger().whileTrue(intake.retractHopperVelocity());
-    // opcon.rightTrigger().onTrue(intake.extendHopper());
-    // opcon.leftTrigger().onTrue(intake.retractHopper());
+    opcon.povUp().whileTrue(intake.extendHopperVelocity());
+    opcon.povDown().whileTrue(intake.retractHopperVelocity());
+    opcon.rightTrigger().onTrue(intake.extendHopper());
+    opcon.leftTrigger().onTrue(intake.retractHopper());
     opcon.rightBumper().whileTrue(intake.intake());
     opcon
         .leftBumper()
@@ -406,7 +406,7 @@ public class RobotContainer extends PeriodicRunnable {
     }
 
     Logger.recordOutput("Time Left", matchTime);
-    Logger.recordOutput("Shift Time Left", timeUntilShiftEnd);
+    Logger.recordOutput("Shift Time Left", Math.round(timeUntilShiftEnd * 100) / 100.0);
     Logger.recordOutput("Current Phase", currentPhase);
   }
 }
