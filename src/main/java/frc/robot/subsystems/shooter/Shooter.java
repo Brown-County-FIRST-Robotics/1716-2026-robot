b032bb27dba2e34a7e87bea604b2a05a191c269d
Quinniboi10 <69563433+Quinniboi10@users.noreply.github.com>
2026-03-28 13:57:03 -0500

    Unstaged changes during deploy at 20260328_135702

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -119,7 +119,8 @@ public class Shooter extends SubsystemBase {
     double numOf100Teeth =
         bGearRotations * 13 + bNumOfTeeth; // How many teeth the 100 gear has rotated
     // Fix because the turret is currently rotated by +1 rotation
-    numOf100Teeth = (numOf100Teeth - 100 + 143) % 143;
+    numOf100Teeth = (numOf100Teeth + 200 + 143) % 143;
+    Logger.recordOutput("turret/encoderEstNumOfTeeth", numOf100Teeth);
     double rotationInRadians = numOf100Teeth / 100 * (2 * Math.PI); // convert to radians
     return Rotation2d.fromRadians(rotationInRadians + (Math.PI) - 1.87);
   }
