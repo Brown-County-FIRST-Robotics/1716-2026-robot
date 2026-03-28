8b36e16dfd6f941c7ae332b5ecfbeea3a546351f
Quinniboi10 <69563433+Quinniboi10@users.noreply.github.com>
2026-03-27 19:54:48 -0500

    Remap controls and add PID locking for translation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -237,7 +237,7 @@ public class RobotContainer extends PeriodicRunnable {
             () -> -controller.getLeftX() / (controller.leftTrigger().getAsBoolean() ? 2 : 1),
             () -> -controller.getRightX() / (controller.leftTrigger().getAsBoolean() ? 2 : 1),
             opcon.a(),
-            controller.a()));
+            controller.rightBumper()));
 
     controller
         .leftStick()
@@ -274,14 +274,14 @@ public class RobotContainer extends PeriodicRunnable {
     shooter.setDefaultCommand(
         Commands.run(() -> shooter.commandTurretToTrack(drive.getPose()), shooter));
 
-    // Switch to X pattern when X button is pressed
-    controller.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
+    // Switch to X pattern when button is pressed
+    controller.leftBumper().onTrue(Commands.runOnce(drive::stopWithX, drive));
 
     // Reset initial pos on auto init
     RobotModeTriggers.autonomous()
         .onTrue(Commands.runOnce(() -> drive.setPose(FieldConstants.flip(initPosChooser.get()))));
-    
-    RobotModeTriggers.disabled().onFalse(Commands.runOnce(() ->drive.resetHold()));
+
+    RobotModeTriggers.disabled().onFalse(Commands.runOnce(drive::resetHold));
 
     new Trigger(intake::isExtenderConnected).onTrue(Commands.runOnce(intake::setZeroPosition));
     RobotModeTriggers.autonomous().onTrue(Commands.runOnce(intake::setZeroPosition));
@@ -328,8 +328,6 @@ public class RobotContainer extends PeriodicRunnable {
                         shooter.getTurretRotation().plus(Rotation2d.fromRotations(-0.1)))));
 
     // Hood positions
-    // controller.y().whileTrue(Commands.run(() -> shooter.quickServoCommand(0), shooter));
-    // controller.b().whileTrue(Commands.run(() -> shooter.quickServoCommand(1), shooter));
     opcon.y().whileTrue(Commands.run(() -> shooter.quickServoCommand(0), shooter));
     opcon.b().whileTrue(Commands.run(() -> shooter.quickServoCommand(1), shooter));
 
@@ -348,39 +346,12 @@ public class RobotContainer extends PeriodicRunnable {
                     Commands.runEnd(
                         () -> rollers.setSpeeds(-20, 0), () -> rollers.setSpeeds(0, 0))));
 
-    // Dpad
-    controller
-        .povUp()
-        .whileTrue(
-            DriveCommands.joystickDriveAtAngle(
-                drive,
-                () -> -controller.getLeftY() / (controller.leftTrigger().getAsBoolean() ? 2 : 1),
-                () -> -controller.getLeftX() / (controller.leftTrigger().getAsBoolean() ? 2 : 1),
-                () -> Rotation2d.kZero));
-    controller
-        .povRight()
-        .whileTrue(
-            DriveCommands.joystickDriveAtAngle(
-                drive,
-                () -> -controller.getLeftY() / (controller.leftTrigger().getAsBoolean() ? 2 : 1),
-                () -> -controller.getLeftX() / (controller.leftTrigger().getAsBoolean() ? 2 : 1),
-                () -> Rotation2d.kCW_90deg));
-    controller
-        .povDown()
-        .whileTrue(
-            DriveCommands.joystickDriveAtAngle(
-                drive,
-                () -> -controller.getLeftY() / (controller.leftTrigger().getAsBoolean() ? 2 : 1),
-                () -> -controller.getLeftX() / (controller.leftTrigger().getAsBoolean() ? 2 : 1),
-                () -> Rotation2d.k180deg));
-    controller
-        .povLeft()
-        .whileTrue(
-            DriveCommands.joystickDriveAtAngle(
-                drive,
-                () -> -controller.getLeftY() / (controller.leftTrigger().getAsBoolean() ? 2 : 1),
-                () -> -controller.getLeftX() / (controller.leftTrigger().getAsBoolean() ? 2 : 1),
-                () -> Rotation2d.kCCW_90deg));
+    // Rotation snapping
+    controller.y().onTrue(Commands.runOnce(() -> drive.targetAngle = Rotation2d.kZero));
+    controller.b().onTrue(Commands.runOnce(() -> drive.targetAngle = Rotation2d.kCW_90deg));
+    controller.a().onTrue(Commands.runOnce(() -> drive.targetAngle = Rotation2d.k180deg));
+    controller.x().onTrue(Commands.runOnce(() -> drive.targetAngle = Rotation2d.kCCW_90deg));
+
     shooter.register();
   }
 
--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -43,8 +43,8 @@ class AngleHoldState {
 
 public class DriveCommands {
   private static final double DEADBAND = 0.1;
-  private static final double DISTANCE_KP = 5.0;
-  private static final double DISTANCE_KD = 0.1;
+  private static final double DISTANCE_KP = 4.0;
+  private static final double DISTANCE_KD = 0.05;
   private static final double ANGLE_KP = 8.0;
   private static final double ANGLE_KD = 0.4;
   private static final double ANGLE_MAX_VELOCITY = 8.0;
@@ -88,6 +88,24 @@ public class DriveCommands {
             ANGLE_KD,
             new TrapezoidProfile.Constraints(ANGLE_MAX_VELOCITY, ANGLE_MAX_ACCELERATION));
     angleController.enableContinuousInput(-Math.PI, Math.PI);
+    angleController.setTolerance(Rotation2d.fromDegrees(5).getRadians());
+
+    ProfiledPIDController distanceControllerX =
+        new ProfiledPIDController(
+            DISTANCE_KP,
+            0.0,
+            DISTANCE_KD,
+            new TrapezoidProfile.Constraints(
+                drive.getMaxLinearSpeedMetersPerSec(), drive.getMaxLinearSpeedMetersPerSec()));
+    ProfiledPIDController distanceControllerY =
+        new ProfiledPIDController(
+            DISTANCE_KP,
+            0.0,
+            DISTANCE_KD,
+            new TrapezoidProfile.Constraints(
+                drive.getMaxLinearSpeedMetersPerSec(), drive.getMaxLinearSpeedMetersPerSec()));
+    distanceControllerX.setTolerance(0.02);
+    distanceControllerY.setTolerance(0.02);
 
     return Commands.run(
             () -> {
@@ -99,17 +117,14 @@ public class DriveCommands {
                 else x = 0;
               }
 
-              // Get linear velocity
-              Translation2d linearVelocity = getLinearVelocityFromJoysticks(x, y);
-
-              // Apply rotation deadband
               double omega = omegaSupplier.getAsDouble();
 
+              // ############ ROTATION AND TRANSLATION HOLDING ############
               if (Math.abs(omega) < DEADBAND) {
                 if (!drive.holdingAngle) {
                   drive.holdingAngle = true;
                   drive.targetAngle = drive.getRotation();
-                  angleController.reset(drive.getRotation().getRadians());
+                  angleController.reset(drive.targetAngle.getRadians());
                   omega = 0;
                 } else
                   omega =
@@ -117,16 +132,38 @@ public class DriveCommands {
                           drive.getRotation().getRadians(), drive.targetAngle.getRadians());
               } else {
                 drive.holdingAngle = false;
-                // Square rotation value for more precise control
                 omega = Math.copySign(omega * omega, omega) * drive.getMaxAngularSpeedRadPerSec();
               }
 
+              if (Math.abs(x) < DEADBAND) {
+                if (!drive.holdingX) {
+                  drive.holdingX = true;
+                  drive.targetX = drive.getTranslation().getX();
+                  distanceControllerX.reset(drive.targetX);
+                  x = 0;
+                } else if (!distanceControllerX.atGoal())
+                  x = distanceControllerX.calculate(drive.getTranslation().getX(), drive.targetX);
+                else x = 0;
+              } else {
+                drive.holdingX = false;
+                x = Math.copySign(x * x, x) * drive.getMaxLinearSpeedMetersPerSec();
+              }
+              if (Math.abs(y) < DEADBAND) {
+                if (!drive.holdingY) {
+                  drive.holdingY = true;
+                  drive.targetY = drive.getTranslation().getY();
+                  distanceControllerY.reset(drive.targetY);
+                  y = 0;
+                } else if (!distanceControllerY.atGoal())
+                  y = distanceControllerY.calculate(drive.getTranslation().getY(), drive.targetY);
+                else y = 0;
+              } else {
+                drive.holdingY = false;
+                y = Math.copySign(y * y, y) * drive.getMaxLinearSpeedMetersPerSec();
+              }
+
               // Convert to field relative speeds & send command
-              ChassisSpeeds speeds =
-                  new ChassisSpeeds(
-                      linearVelocity.getX() * drive.getMaxLinearSpeedMetersPerSec(),
-                      linearVelocity.getY() * drive.getMaxLinearSpeedMetersPerSec(),
-                      omega);
+              ChassisSpeeds speeds = new ChassisSpeeds(x, y, omega);
               boolean isFlipped =
                   DriverStation.getAlliance().isPresent()
                       && DriverStation.getAlliance().get() == Alliance.Red;
@@ -141,7 +178,11 @@ public class DriveCommands {
                             : drive.getPose().getRotation()));
             },
             drive)
-        .beforeStarting(() -> angleController.reset(drive.getRotation().getRadians()));
+        .beforeStarting(
+            () -> {
+              angleController.reset(drive.getRotation().getRadians());
+              drive.resetHold();
+            });
   }
 
   /**
