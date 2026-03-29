2a0acedb2523501a89008c65d39b704e35b30a4a
linnea <lkennerhed@gmail.com>
2026-03-29 14:29:41 -0500

    Unstaged changes during deploy at 20260329_142941

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -146,34 +146,8 @@ public class DriveCommands {
                 omega = Math.copySign(omega * omega, omega) * drive.getMaxAngularSpeedRadPerSec();
               }
 
-              if (Math.abs(x) < DEADBAND) {
-                if (!drive.holdingX) {
-                  drive.holdingX = true;
-                  drive.targetX =
-                      drive.getTranslation().getX() + currentSpeeds.vxMetersPerSecond * LOOKAHEAD_X;
-                  distanceControllerX.reset(drive.targetX);
-                  x = 0;
-                } else if (!distanceControllerX.atGoal())
-                  x = distanceControllerX.calculate(drive.getTranslation().getX(), drive.targetX);
-                else x = 0;
-              } else {
-                drive.holdingX = false;
-                x = Math.copySign(x * x, x) * drive.getMaxLinearSpeedMetersPerSec();
-              }
-              if (Math.abs(y) < DEADBAND) {
-                if (!drive.holdingY) {
-                  drive.holdingY = true;
-                  drive.targetY =
-                      drive.getTranslation().getY() + currentSpeeds.vyMetersPerSecond * LOOKAHEAD_Y;
-                  distanceControllerY.reset(drive.targetY);
-                  y = 0;
-                } else if (!distanceControllerY.atGoal())
-                  y = distanceControllerY.calculate(drive.getTranslation().getY(), drive.targetY);
-                else y = 0;
-              } else {
-                drive.holdingY = false;
-                y = Math.copySign(y * y, y) * drive.getMaxLinearSpeedMetersPerSec();
-              }
+              x = Math.copySign(x * x, x) * drive.getMaxLinearSpeedMetersPerSec();
+              y = Math.copySign(y * y, y) * drive.getMaxLinearSpeedMetersPerSec();
 
               // Convert to field relative speeds & send command
               ChassisSpeeds speeds = new ChassisSpeeds(x, y, omega);
