7c7e0938d9409befdd1565f33bd2051a3bd7db11
Quinniboi10 <69563433+Quinniboi10@users.noreply.github.com>
2026-03-29 17:57:42 -0500

    Unstaged changes during deploy at 20260329_175741

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -355,8 +355,8 @@ public class RobotContainer extends PeriodicRunnable {
                         shooter.getTurretRotation().plus(Rotation2d.fromRotations(-0.1)))));
 
     // Hood positions
-    opcon.y().whileTrue(Commands.run(() -> shooter.quickServoCommand(0), shooter));
-    opcon.b().whileTrue(Commands.run(() -> shooter.quickServoCommand(1), shooter));
+    opcon.y().whileTrue(Commands.run(() -> shooter.quickServoCommand(0.4), shooter));
+    opcon.b().whileTrue(Commands.run(() -> shooter.quickServoCommand(0.5), shooter));
 
     // Intake/hopper control
     opcon.povUp().whileTrue(intake.extendHopperVelocity());
