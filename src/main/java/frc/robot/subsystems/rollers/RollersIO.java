dcaa0e52d01b646079cc5388500131a09f4464ad
Quinniboi10 <69563433+Quinniboi10@users.noreply.github.com>
2026-03-30 14:25:30 -0500

    Unstaged changes during deploy at 20260330_142530

--- src/main/java/frc/robot/subsystems/rollers/RollersIO.java
+++ src/main/java/frc/robot/subsystems/rollers/RollersIO.java
@@ -9,13 +9,20 @@ public interface RollersIO {
     public boolean rollersConnected = false;
     public double rollerAppliedVolts = 0.0;
     public double rollerAppliedCurrent = 0.0;
+
     public double kickerVelocity = 0.0;
     public boolean kickerConnected = false;
     public double kickerAppliedVolts = 0.0;
     public double kickerAppliedCurrent = 0.0;
+
+    public double kicklerVelocity = 0.0;
+    public boolean kicklerConnected = false;
+    public double kicklerAppliedVolts = 0.0;
+    public double kicklerAppliedCurrent = 0.0;
   }
 
   default void updateInputs(RollersIOInputs inputs) {}
 
-  default void commandSpeed(double rollerVelocity, double kicker_velocity) {}
+  default void commandSpeed(
+      double kickler_velocity, double roller_velocity, double kicker_velocity) {}
 }
