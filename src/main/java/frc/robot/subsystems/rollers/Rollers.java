8d06599e31edf67986f549779fa870ca0d57fb25
Quinniboi10 <69563433+Quinniboi10@users.noreply.github.com>
2026-03-30 14:33:41 -0500

    Unstaged changes during deploy at 20260330_143341

--- src/main/java/frc/robot/subsystems/rollers/Rollers.java
+++ src/main/java/frc/robot/subsystems/rollers/Rollers.java
@@ -9,10 +9,13 @@ import org.littletonrobotics.junction.Logger;
 public class Rollers extends SubsystemBase {
   RollersIO io;
   RollersIOInputsAutoLogged inputs = new RollersIOInputsAutoLogged();
+  private final Debouncer kicklerConnectedDebouncer =
+      new Debouncer(OurConstants.CONNECTED_DEBOUNCE_TIME, Debouncer.DebounceType.kFalling);
   private final Debouncer rollerConnectedDebouncer =
       new Debouncer(OurConstants.CONNECTED_DEBOUNCE_TIME, Debouncer.DebounceType.kFalling);
   private final Debouncer kickerConnectedDebouncer =
       new Debouncer(OurConstants.CONNECTED_DEBOUNCE_TIME, Debouncer.DebounceType.kFalling);
+  private final Alert kicklerDisconnectedAlert;
   private final Alert rollerDisconnectedAlert;
   private final Alert kickerDisconnectedAlert;
 
@@ -20,18 +23,20 @@ public class Rollers extends SubsystemBase {
     this.io = io;
     rollerDisconnectedAlert =
         new Alert("T.I.C.K.L.E.R. motor disconnected", Alert.AlertType.kError);
+    kicklerDisconnectedAlert = new Alert("Kickler. motor disconnected", Alert.AlertType.kError);
     kickerDisconnectedAlert = new Alert("Kicker motor disconnected", Alert.AlertType.kError);
   }
 
-  public void setSpeeds(double rollerVelocity, double kicker_velocity) {
-    io.commandSpeed(rollerVelocity, kicker_velocity);
+  public void setSpeeds(double kicklerVelocity, double rollerVelocity, double kicker_velocity) {
+    io.commandSpeed(kicklerVelocity, rollerVelocity, kicker_velocity);
   }
 
   @Override
   public void periodic() {
     io.updateInputs(inputs);
     Logger.processInputs("rollers", inputs);
-    kickerDisconnectedAlert.set(!kickerConnectedDebouncer.calculate(inputs.kickerConnected));
+    kicklerDisconnectedAlert.set(!kicklerConnectedDebouncer.calculate(inputs.kicklerConnected));
     rollerDisconnectedAlert.set(!rollerConnectedDebouncer.calculate(inputs.rollersConnected));
+    kickerDisconnectedAlert.set(!kickerConnectedDebouncer.calculate(inputs.kickerConnected));
   }
 }
--- src/main/java/frc/robot/subsystems/rollers/RollersIOKraken.java
+++ src/main/java/frc/robot/subsystems/rollers/RollersIOKraken.java
@@ -5,9 +5,11 @@ import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.ClosedLoopRampsConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
@@ -17,24 +19,44 @@ import frc.robot.OurConstants;
 public class RollersIOKraken implements RollersIO {
   TalonFX rollerMotor;
   TalonFX kickerMotor;
+  TalonFX kicklerMotor;
   StatusSignal<AngularVelocity> rollerVelocity;
-  StatusSignal<Current> rollerCurrent;
   StatusSignal<Voltage> rollerAppliedVolts;
+  StatusSignal<Current> rollerCurrent;
   StatusSignal<AngularVelocity> kickerVelocity;
-  StatusSignal<Current> kickerCurrent;
   StatusSignal<Voltage> kickerAppliedVolts;
+  StatusSignal<Current> kickerCurrent;
+  StatusSignal<AngularVelocity> kicklerVelocity;
+  StatusSignal<Voltage> kicklerAppliedVolts;
+  StatusSignal<Current> kicklerCurrent;
 
-  public RollersIOKraken(int rollerID, int kickerID) {
+  public RollersIOKraken(int kicklerID, int rollerID, int kickerID) {
+    kicklerMotor = new TalonFX(kicklerID, OurConstants.CAN_BUS);
     rollerMotor = new TalonFX(rollerID, OurConstants.CAN_BUS);
     kickerMotor = new TalonFX(kickerID, OurConstants.CAN_BUS);
+
     rollerVelocity = rollerMotor.getVelocity();
     rollerCurrent = rollerMotor.getStatorCurrent();
     rollerCurrent.setUpdateFrequency(50);
     rollerAppliedVolts = rollerMotor.getMotorVoltage();
+
     kickerVelocity = kickerMotor.getVelocity();
     kickerCurrent = kickerMotor.getStatorCurrent();
     kickerCurrent.setUpdateFrequency(50);
     kickerAppliedVolts = kickerMotor.getMotorVoltage();
+
+    kicklerVelocity = kicklerMotor.getVelocity();
+    kicklerAppliedVolts = kicklerMotor.getMotorVoltage();
+    kicklerCurrent = kicklerMotor.getStatorCurrent();
+    kicklerCurrent.setUpdateFrequency(50);
+
+    var kickler_cfg = new TalonFXConfiguration();
+    kickler_cfg.Slot0 = new Slot0Configs().withKV(12.0 / 100.0).withKP(12.0 / 100.0);
+    kickler_cfg.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+    kickler_cfg.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+
+    kicklerMotor.getConfigurator().apply(kickler_cfg);
+
     var kicker_cfgr = kickerMotor.getConfigurator();
     kicker_cfgr.apply(
         new ClosedLoopRampsConfigs()
@@ -71,19 +93,30 @@ public class RollersIOKraken implements RollersIO {
         BaseStatusSignal.refreshAll(rollerCurrent, rollerAppliedVolts, rollerVelocity);
     var kickerStatus =
         BaseStatusSignal.refreshAll(kickerCurrent, kickerAppliedVolts, kickerVelocity);
-    inputs.kickerConnected = kickerStatus.isOK();
-    inputs.rollersConnected = rollerStatus.isOK();
-    inputs.kickerAppliedVolts = kickerAppliedVolts.getValueAsDouble();
-    inputs.kickerVelocity = kickerVelocity.getValue().in(Units.RotationsPerSecond);
-    inputs.kickerAppliedCurrent = kickerCurrent.getValueAsDouble();
-    inputs.rollerAppliedVolts = rollerAppliedVolts.getValueAsDouble();
+    var kicklerStatus =
+        BaseStatusSignal.refreshAll(kicklerCurrent, kicklerAppliedVolts, kickerVelocity);
+
     inputs.rollerVelocity = rollerVelocity.getValue().in(Units.RotationsPerSecond);
+    inputs.rollersConnected = rollerStatus.isOK();
+    inputs.rollerAppliedVolts = rollerAppliedVolts.getValueAsDouble();
     inputs.rollerAppliedCurrent = rollerCurrent.getValueAsDouble();
+
+    inputs.kickerVelocity = kickerVelocity.getValue().in(Units.RotationsPerSecond);
+    inputs.kickerConnected = kickerStatus.isOK();
+    inputs.kickerAppliedVolts = kickerAppliedVolts.getValueAsDouble();
+    inputs.kickerAppliedCurrent = kickerCurrent.getValueAsDouble();
+
+    inputs.kicklerVelocity = kicklerVelocity.getValue().in(Units.RotationsPerSecond);
+    inputs.kicklerConnected = kicklerStatus.isOK();
+    inputs.kicklerAppliedVolts = kicklerAppliedVolts.getValueAsDouble();
+    inputs.kicklerAppliedCurrent = kicklerCurrent.getValueAsDouble();
   }
 
   @Override
-  public void commandSpeed(double rollerVelocity, double kicker_velocity) {
+  public void commandSpeed(
+      double kickler_velocity, double roller_velocity, double kicker_velocity) {
+    kicklerMotor.setControl(new VelocityVoltage(kicker_velocity));
+    rollerMotor.setControl(new VelocityVoltage(roller_velocity));
     kickerMotor.setControl(new VelocityVoltage(kicker_velocity));
-    rollerMotor.setControl(new VelocityVoltage(rollerVelocity));
   }
 }
