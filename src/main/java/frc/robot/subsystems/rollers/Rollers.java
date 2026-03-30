ff174c115089487e109c2eda0243971a21610201
Quinniboi10 <69563433+Quinniboi10@users.noreply.github.com>
2026-03-30 16:46:49 -0500

    Write kickler code

--- src/main/java/frc/robot/subsystems/rollers/Rollers.java
+++ src/main/java/frc/robot/subsystems/rollers/Rollers.java
@@ -9,29 +9,33 @@ import org.littletonrobotics.junction.Logger;
 public class Rollers extends SubsystemBase {
   RollersIO io;
   RollersIOInputsAutoLogged inputs = new RollersIOInputsAutoLogged();
-  private final Debouncer rollerConnectedDebouncer =
+  private final Debouncer beltsConnectedDebouncer =
+      new Debouncer(OurConstants.CONNECTED_DEBOUNCE_TIME, Debouncer.DebounceType.kFalling);
+  private final Debouncer starsConnectedDebouncer =
       new Debouncer(OurConstants.CONNECTED_DEBOUNCE_TIME, Debouncer.DebounceType.kFalling);
   private final Debouncer kickerConnectedDebouncer =
       new Debouncer(OurConstants.CONNECTED_DEBOUNCE_TIME, Debouncer.DebounceType.kFalling);
-  private final Alert rollerDisconnectedAlert;
+  private final Alert beltsDisconnectedAlert;
+  private final Alert starsDisconnectedAlert;
   private final Alert kickerDisconnectedAlert;
 
   public Rollers(RollersIO io) {
     this.io = io;
-    rollerDisconnectedAlert =
-        new Alert("T.I.C.K.L.E.R. motor disconnected", Alert.AlertType.kError);
+    starsDisconnectedAlert = new Alert("Star wheel motor disconnected", Alert.AlertType.kError);
+    beltsDisconnectedAlert = new Alert("Belt motor disconnected", Alert.AlertType.kError);
     kickerDisconnectedAlert = new Alert("Kicker motor disconnected", Alert.AlertType.kError);
   }
 
-  public void setSpeeds(double rollerVelocity, double kicker_velocity) {
-    io.commandSpeed(rollerVelocity, kicker_velocity);
+  public void setSpeeds(double beltsVelocity, double starsVelocity, double kicker_velocity) {
+    io.commandSpeed(beltsVelocity, starsVelocity, kicker_velocity);
   }
 
   @Override
   public void periodic() {
     io.updateInputs(inputs);
     Logger.processInputs("rollers", inputs);
+    beltsDisconnectedAlert.set(!beltsConnectedDebouncer.calculate(inputs.beltsConnected));
+    starsDisconnectedAlert.set(!starsConnectedDebouncer.calculate(inputs.starsConnected));
     kickerDisconnectedAlert.set(!kickerConnectedDebouncer.calculate(inputs.kickerConnected));
-    rollerDisconnectedAlert.set(!rollerConnectedDebouncer.calculate(inputs.rollersConnected));
   }
 }
